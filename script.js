42c9f9677b56303ed67cadec1a454f2a339b5cca
Ian Kesler <ianmkesler@gmail.com>
2019-07-17 14:15:53 -0700

    Add new sketchpad button and functionality

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ function createGrid(rows,columns){
     for(let i = 0; i<(rows*columns); i++){
         const item = document.createElement("div");
         item.classList.add("grid-item");
-        //item.id.add(i);
 
         container.appendChild(item);
 
+        container.style.cssText = "grid-template-columns: repeat("+columns+ ","+columns+"fr); grid-template-rows: repeat("+rows+","+rows+"fr);";
+
+    }
+}
+
+function deleteGrid(){
+    var item = container.lastElementChild;
+
+    while(item){
+        container.removeChild(item);
+        item = container.lastElementChild;
     }
 }
 
@@ -20,9 +30,22 @@ function addListeners(grid,effect){
     });
 }
 
+function newSketchPad(event){
+    let rows = prompt("How many rows?");
+    let columns = prompt("How many columns?");
+
+    deleteGrid();
+    createGrid(rows,columns);
+    grid = document.querySelectorAll("div.grid-item"); //update grid, not "live"
+    addListeners(grid,draw);
+}
+
 
 const container = document.querySelector(".grid-container");
 createGrid(16,16);
 
 var grid = document.querySelectorAll("div.grid-item")
 addListeners(grid,draw);
+
+var reset = document.querySelector('.reset');
+reset.addEventListener('click', newSketchPad);
