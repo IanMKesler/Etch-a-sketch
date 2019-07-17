b5aa4e507702d8256ca66bcdba052b4a516141fb
Ian Kesler <ianmkesler@gmail.com>
2019-07-17 12:27:34 -0700

    Add event listeners

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 function createGrid(rows,columns){
-    const container = document.querySelector(".grid-container");
-
-    
-
-
     for(let i = 0; i<(rows*columns); i++){
         const item = document.createElement("div");
         item.classList.add("grid-item");
+        //item.id.add(i);
 
         container.appendChild(item);
 
     }
 }
 
+function draw(event){
+    this.style.cssText = "background-color: black";
+
+}
+
+function addListeners(grid,effect){
+    grid.forEach( (item) => {
+        item.addEventListener('mouseover', effect);
+    });
+}
 
 
+const container = document.querySelector(".grid-container");
 createGrid(16,16);
+
+var grid = document.querySelectorAll("div.grid-item")
+addListeners(grid,draw);
