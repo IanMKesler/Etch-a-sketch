0fabf737771c0beca1723f18a5829db732551970
Ian Kesler <ianmkesler@gmail.com>
2019-07-17 18:42:59 -0700

    Add eraser and realtime button controlled drawstate

--- script.js
+++ script.js
@@ -51,22 +51,33 @@ function shadeRGB(style){
 
 }
 
-function draw(event){
-
-    //this.style.cssText = "background-color: rgb("+randRGB()+");";
-
+function shade(event){
     let style = this.style.cssText;
     shadeRGB(style);
     this.style.cssText = "background-color: rgb("+shadeRGB(style)+");";
+}
 
+function black(event){
+    this.style.cssText = "background-color: rgb(0,0,0);";
+}
 
-    //this.style.cssText = "background-color: rgb(0,0,0);";
+function random(event){
+    this.style.cssText = "background-color: rgb("+randRGB()+");";
+}
 
+function erase(event){
+    this.style.cssText = "background-color: rgb(255,255,255);";
 }
 
 function addListeners(grid,effect){
     grid.forEach( (item) => {
         item.addEventListener('mouseover', effect);
+    });
+}
+
+function removeListeners(grid,effect){
+    grid.forEach( (item) => {
+        item.removeEventListener('mouseover', effect);
     });
 }
 
@@ -77,7 +88,31 @@ function newSketchPad(event){
     deleteGrid();
     createGrid(rows,columns);
     grid = document.querySelectorAll("div.grid-item"); //update grid, not "live"
-    addListeners(grid,draw);
+    addListeners(grid,drawState); 
+}
+
+function replaceListeners(event){
+    removeListeners(grid,drawState);
+    let button = this.classList.item(0);
+    switch(true){
+        case (button =="black"):
+            addListeners(grid,black);
+            drawState = black;
+            break;
+        case (button =="shade"):
+            addListeners(grid,shade);
+            drawState = shade;
+            break;
+        case (button == "random"):
+            addListeners(grid,random);
+            drawState = random;
+            break;
+        case (button == "erase"):
+            addListeners(grid,erase);
+            drawState = erase;
+            break;
+    }
+    
 }
 
 
@@ -85,7 +120,21 @@ const container = document.querySelector(".grid-container");
 createGrid(16,16);
 
 var grid = document.querySelectorAll("div.grid-item")
-addListeners(grid,draw);
+addListeners(grid,black);
 
-var reset = document.querySelector('.reset');
-reset.addEventListener('click', newSketchPad);
+var drawState = black;
+
+var resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', newSketchPad);
+
+var blackButton = document.querySelector('.black');
+blackButton.addEventListener('click', replaceListeners);
+
+var shadeButton = document.querySelector('.shade');
+shadeButton.addEventListener('click', replaceListeners);
+
+var randomButton = document.querySelector('.random');
+randomButton.addEventListener('click', replaceListeners);
+
+var eraseButton = document.querySelector('.erase');
+eraseButton.addEventListener('click', replaceListeners);
